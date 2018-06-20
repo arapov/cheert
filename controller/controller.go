ddad3cc727160650fd0861ad3fd96d9bb9a43879
Anton Arapov <arapov@gmail.com>
2018-06-20 16:32:04 +0200

    remove unneeded routes
    Signed-off-by: Anton Arapov <arapov@gmail.com>

--- controller/controller.go
+++ controller/controller.go
@@ -3,23 +3,18 @@ package controller
 
 import (
 	"github.com/arapov/cheert/controller/about"
-	"github.com/arapov/cheert/controller/debug"
 	"github.com/arapov/cheert/controller/home"
-	"github.com/arapov/cheert/controller/login"
-	"github.com/arapov/cheert/controller/notepad"
-	"github.com/arapov/cheert/controller/register"
 	"github.com/arapov/cheert/controller/static"
-	"github.com/arapov/cheert/controller/status"
 )
 
 // LoadRoutes loads the routes for each of the controllers.
 func LoadRoutes() {
 	about.Load()
-	debug.Load()
-	register.Load()
-	login.Load()
+	//	debug.Load()
+	//	register.Load()
+	//	login.Load()
 	home.Load()
 	static.Load()
-	status.Load()
-	notepad.Load()
+	//	status.Load()
+	//	notepad.Load()
 }
