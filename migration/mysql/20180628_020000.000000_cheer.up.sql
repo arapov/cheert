b25ca06750344eea1ad554395fdbae416e6424f3
Anton Arapov <arapov@gmail.com>
2018-06-28 19:27:16 +0200

    fixup
    Signed-off-by: Anton Arapov <arapov@gmail.com>

--- migration/mysql/20180628_020000.000000_cheer.up.sql
+++ migration/mysql/20180628_020000.000000_cheer.up.sql
@@ -4,10 +4,10 @@ SET time_zone = '+00:00';
 CREATE TABLE cheers (
     id INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
     
-    from TEXT NOT NULL,
-    to TEXT NOT NULL,
-    plus TEXT NOT NULL,
-    note TEXT NOT NULL,
+    `from` TEXT NOT NULL,
+    `to` TEXT NOT NULL,
+    `plus` TEXT NOT NULL,
+    `note` TEXT NOT NULL,
     
     created_at TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
