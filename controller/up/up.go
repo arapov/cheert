ff4c92f5c1e4a4bcf2e2826787ca2d383916a3a2
Anton Arapov <arapov@gmail.com>
2018-10-18 13:16:12 +0200

    push pnt!
    Signed-off-by: Anton Arapov <arapov@gmail.com>

--- controller/up/up.go
+++ controller/up/up.go
@@ -15,9 +15,47 @@ import (
 
 // Load the routes.
 func Load() {
-	router.Post("/up", Index)
+	router.Post("/up", Index2)
 	router.Post("/up/submit", Submit)
 	router.Get("/up/submit", End)
+}
+
+// Index displays the home page.
+func Index2(w http.ResponseWriter, r *http.Request) {
+	c := flight.Context(w, r)
+	v := c.View.New("up/index")
+
+	dfgs := []string{
+		"ds-core",
+		"cs-core",
+		"sssd",
+		"freeipa",
+		"freeipa-ds",
+		"freeipa-cs-in-ipa",
+		"crypto",
+		"audit-subsystem",
+		"selinux-solutions",
+		"security-compliance",
+		"security-technologies",
+		"common-logging-and-session-recording",
+		"openstack-identity-and-security-engineering",
+		"special-projects",
+		"special-projects-and-integration",
+		"samba",
+	}
+
+	var dfgComma string
+	for _, dfg := range dfgs {
+		if strings.Contains(r.FormValue("group"), dfg) {
+			dfgComma += dfg + ","
+		}
+	}
+
+	v.Vars["group"] = strings.TrimSuffix(dfgComma, ",")
+	v.Vars["id"] = r.FormValue("id")
+	v.Vars["uid"] = r.FormValue("uid")
+
+	v.Render(w, r)
 }
 
 // Index displays the home page.
