4e53b95196c4a0592bfc7e79e1f77075ea6445ab
Anton Arapov <arapov@gmail.com>
2018-10-18 14:20:00 +0200

    pnt!
    Signed-off-by: Anton Arapov <arapov@gmail.com>

--- controller/up/up.go
+++ controller/up/up.go
@@ -2,9 +2,7 @@
 package up
 
 import (
-	"fmt"
 	"net/http"
-	"regexp"
 	"strings"
 
 	"github.com/arapov/cheert/lib/flight"
@@ -58,56 +56,11 @@ func Index2(w http.ResponseWriter, r *http.Request) {
 	v.Render(w, r)
 }
 
-// Index displays the home page.
-func Index(w http.ResponseWriter, r *http.Request) {
-	c := flight.Context(w, r)
-	v := c.View.New("up/index")
-
-	re, _ := regexp.Compile(`rhos-dfg-[a-z-]*`)
-	dfgs := re.FindAllStringSubmatch(r.FormValue("group"), -1)
-
-	v.Vars["group"] = ""
-	if len(dfgs) > 0 {
-
-		var dfgComma string
-		check := make(map[string]int)
-		for _, dfg := range dfgs {
-			squad := strings.Index(dfg[0], "-squad")
-			if squad != -1 {
-				if _, ok := check[dfg[0][0:squad]]; !ok {
-					check[dfg[0][0:squad]] = 1
-					dfgComma += fmt.Sprintf("%s,", dfg[0][0:squad])
-				}
-			} else {
-				if _, ok := check[dfg[0]]; !ok {
-					check[dfg[0]] = 1
-					dfgComma += fmt.Sprintf("%s,", dfg[0])
-				}
-			}
-		}
-		dfgComma = dfgComma[0 : len(dfgComma)-1]
-		v.Vars["group"] = dfgComma
-	}
-
-	v.Vars["id"] = r.FormValue("id")
-	v.Vars["uid"] = r.FormValue("uid")
-
-	v.Render(w, r)
-}
-
 func Submit(w http.ResponseWriter, r *http.Request) {
 	c := flight.Context(w, r)
 
 	r.ParseForm()
-	/*
-		for k, v := range r.Form {
-			log.Println(k, " > ", v)
-		}
-	*/
 	for i, value := range r.Form["uid"] {
-		//log.Println(value, " praise ", r.Form["praise"][i])
-		//log.Println(value, " plus ", r.Form["plus["+value+"]"])
-		//up.Create(c.DB, value, r.Form["from"][0], r.Form["plus["+value+"]"][0], r.Form["praise"][i])
 		plus := "0"
 		if _, ok := r.Form["plus["+value+"]"]; ok {
 			plus = r.Form["plus["+value+"]"][0] //do something here
@@ -120,6 +73,7 @@ func Submit(w http.ResponseWriter, r *http.Request) {
 		}
 
 	}
+
 }
 
 func End(w http.ResponseWriter, r *http.Request) {
